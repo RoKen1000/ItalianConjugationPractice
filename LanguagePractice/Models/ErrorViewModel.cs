b73e16822e29423dbb1cfdfd7b3fb419f9de2aff
RoKen1000 <rosskennedy993@outlook.com>
2023-09-24 16:00:54 +0100

    Add project files.

---
+++ LanguagePractice/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LanguagePractice.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
