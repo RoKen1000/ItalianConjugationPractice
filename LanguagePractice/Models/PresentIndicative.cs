318549f76a704b0bfc4d2eeac1b08f9e956f7bf9
RoKen1000 <rosskennedy993@outlook.com>
2023-09-25 14:40:38 +0100

    Create model for Present Indicative tense.

---
+++ LanguagePractice/Models/PresentIndicative.cs
@@ -0,0 +1,14 @@
+﻿namespace LanguagePractice.Models
+{
+    public class PresentIndicative
+    {
+        public int Id { get; set; }
+        public string Infinitive { get; set; }
+        public string FirstPersonSingular { get; set; }
+        public string SecondPersonSingular { get; set; }
+        public string ThirdPersonSingular { get; set; }
+        public string FirstPersonPlural { get; set; }
+        public string SecondPersonPlural { get; set; }
+        public string ThirdPersonPlural { get; set; }
+    }
+}
