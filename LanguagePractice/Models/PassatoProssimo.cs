783293bb4eaee161023d8e3beeecd0a4b66b2db1
RoKen1000 <rosskennedy993@outlook.com>
2023-09-28 14:14:06 +0100

    Create PassatoProssimo class.

---
+++ LanguagePractice/Models/PassatoProssimo.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LanguagePracticeSite.Models
+{
+    public class PassatoProssimo
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Infinitive { get; set; }
+        [Required]
+        public bool UsesEssere { get; set; }
+        [Required]
+        public string FirstPersonSingular { get; set; }
+        [Required]
+        public string SecondPersonSingular { get; set; }
+        [Required]
+        public string ThirdPersonSingular { get; set; }
+        [Required]
+        public string FirstPersonPlural { get; set; }
+        [Required]
+        public string SecondPersonPlural { get; set; }
+        [Required]
+        public string ThirdPersonPlural { get; set; }
+    }
+}
