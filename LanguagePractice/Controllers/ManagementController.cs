bd98d1938d45dc553b98c38994a782694b1a55c1
RoKen1000 <rosskennedy993@outlook.com>
2023-09-29 15:21:44 +0100

    Create Create action method in ManagementController and create its view. Create link to this in Management Index view.

--- LanguagePractice/Controllers/ManagementController.cs
+++ LanguagePractice/Controllers/ManagementController.cs
@@ -1,10 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LanguagePractice.DataAccess.DataContext;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LanguagePracticeSite.Controllers
 {
     public class ManagementController : Controller
     {
+        private readonly WordDatabaseContext _db;
+        public ManagementController(WordDatabaseContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
         {
             return View();
         }
