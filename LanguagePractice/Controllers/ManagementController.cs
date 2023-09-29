a61864e54cff90c93b89c39837d1139338b635f5
RoKen1000 <rosskennedy993@outlook.com>
2023-09-29 15:13:04 +0100

    Create ManagementController and Management Index view. Add link in Home Index to Management Index. Remove redundant using statement in WordDatabaseContext.

---
+++ LanguagePractice/Controllers/ManagementController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LanguagePracticeSite.Controllers
+{
+    public class ManagementController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
