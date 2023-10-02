e9f6e63ff33d37ebdc03cb6ea5b2583ec5f92527
RoKen1000 <rosskennedy993@outlook.com>
2023-10-02 10:59:00 +0100

    Removed accented characters from seed data in WordDatabaseContext. Create data migrations and update database to implement changes. After having discussed whether accented characters are actually used in Italian with an Italian acquaintance, I was told that accented characters are rarely used in Italian verbs and using accented characters is an archaic form of writing verbs. Accented characters are only used in certain words where there can be multiple different meanings for the same word.

--- LanguagePractice/Models/DataContext/WordDatabaseContext.cs
+++ LanguagePractice/Models/DataContext/WordDatabaseContext.cs
@@ -17,21 +17,21 @@ namespace LanguagePractice.DataAccess.DataContext
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<PresentIndicative>().HasData(
-                    new PresentIndicative { Id = 1, Infinitive = "fàre", FirstPersonSingular = "fàccio", SecondPersonSingular = "fài", ThirdPersonSingular = "fà", FirstPersonPlural = "facciàmo", SecondPersonPlural = "fàte", ThirdPersonPlural = "fànno"},
-                    new PresentIndicative { Id = 2, Infinitive = "vedére", FirstPersonSingular = "védo", SecondPersonSingular = "védi", ThirdPersonSingular = "véde", FirstPersonPlural = "vediàmo", SecondPersonPlural = "vedéte", ThirdPersonPlural = "védono"},
-                    new PresentIndicative { Id = 3, Infinitive = "parlàre", FirstPersonSingular = "pàrlo", SecondPersonSingular = "pàrli", ThirdPersonSingular = "pàrla", FirstPersonPlural = "parliàmo", SecondPersonPlural = "parlàte", ThirdPersonPlural = "pàrlano"}
+                    new PresentIndicative { Id = 1, Infinitive = "fare", FirstPersonSingular = "faccio", SecondPersonSingular = "fai", ThirdPersonSingular = "fa", FirstPersonPlural = "facciamo", SecondPersonPlural = "fate", ThirdPersonPlural = "fanno"},
+                    new PresentIndicative { Id = 2, Infinitive = "vedere", FirstPersonSingular = "vedo", SecondPersonSingular = "vedi", ThirdPersonSingular = "vede", FirstPersonPlural = "vediamo", SecondPersonPlural = "vedete", ThirdPersonPlural = "vedono"},
+                    new PresentIndicative { Id = 3, Infinitive = "parlare", FirstPersonSingular = "parlo", SecondPersonSingular = "parli", ThirdPersonSingular = "parla", FirstPersonPlural = "parliamo", SecondPersonPlural = "parlate", ThirdPersonPlural = "parlano"}
                 );
 
             modelBuilder.Entity<PresentPerfect>().HasData(
-                    new PresentPerfect { Id = 1, Infinitive = "preparàre", FirstPersonSingular = "ho preparato", SecondPersonSingular = "hai preparato", ThirdPersonSingular = "ha preparato", FirstPersonPlural = "abbiamo preparato", SecondPersonPlural = "avete preparato", ThirdPersonPlural = "hanno preparato", UsesEssere = false },
-                    new PresentPerfect { Id = 2, Infinitive = "véndere", FirstPersonSingular = "ho venduto", SecondPersonSingular = "hai venduto", ThirdPersonSingular = "ha venduto", FirstPersonPlural = "abbiamo venduto", SecondPersonPlural = "avete venduto", ThirdPersonPlural = "hanno venduto", UsesEssere = false},
+                    new PresentPerfect { Id = 1, Infinitive = "preparare", FirstPersonSingular = "ho preparato", SecondPersonSingular = "hai preparato", ThirdPersonSingular = "ha preparato", FirstPersonPlural = "abbiamo preparato", SecondPersonPlural = "avete preparato", ThirdPersonPlural = "hanno preparato", UsesEssere = false },
+                    new PresentPerfect { Id = 2, Infinitive = "vendere", FirstPersonSingular = "ho venduto", SecondPersonSingular = "hai venduto", ThirdPersonSingular = "ha venduto", FirstPersonPlural = "abbiamo venduto", SecondPersonPlural = "avete venduto", ThirdPersonPlural = "hanno venduto", UsesEssere = false},
                     new PresentPerfect { Id = 3, Infinitive = "andare", FirstPersonSingular = "sono andato", SecondPersonSingular = "sei andato", ThirdPersonSingular = "è andato", FirstPersonPlural = "siamo andati", SecondPersonPlural = "siete andati", ThirdPersonPlural = "sono andati", UsesEssere = true}
                 );
 
             modelBuilder.Entity<Imperfect>().HasData(
-                    new Imperfect { Id = 1, Infinitive = "vedere", FirstPersonSingular = "védevo", SecondPersonSingular = "vedévi", ThirdPersonSingular = "vedéva", FirstPersonPlural = "vedevàmo", SecondPersonPlural = "vedevàte", ThirdPersonPlural = "vedévano"},
-                    new Imperfect { Id = 2, Infinitive = "parlàre", FirstPersonSingular = "parlàvo", SecondPersonSingular = "parlàvi", ThirdPersonSingular = "parlàva", FirstPersonPlural = "parlavàmo", SecondPersonPlural = "parlavàte", ThirdPersonPlural = "parlàvano" },
-                    new Imperfect { Id = 3, Infinitive = "venìre", FirstPersonSingular = "venìvo", SecondPersonSingular = "venìvi", ThirdPersonSingular = "venìva", FirstPersonPlural = "venivàmo", SecondPersonPlural = "venivàte", ThirdPersonPlural = "venìvano" }
+                    new Imperfect { Id = 1, Infinitive = "vedere", FirstPersonSingular = "vedevo", SecondPersonSingular = "vedevi", ThirdPersonSingular = "vedeva", FirstPersonPlural = "vedevamo", SecondPersonPlural = "vedevate", ThirdPersonPlural = "vedevano"},
+                    new Imperfect { Id = 2, Infinitive = "parlare", FirstPersonSingular = "parlavo", SecondPersonSingular = "parlavi", ThirdPersonSingular = "parlava", FirstPersonPlural = "parlavamo", SecondPersonPlural = "parlavate", ThirdPersonPlural = "parlavano" },
+                    new Imperfect { Id = 3, Infinitive = "venire", FirstPersonSingular = "venivo", SecondPersonSingular = "venivi", ThirdPersonSingular = "veniva", FirstPersonPlural = "venivamo", SecondPersonPlural = "venivate", ThirdPersonPlural = "venivano" }
                 );
         }
     }
