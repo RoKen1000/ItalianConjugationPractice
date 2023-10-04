7a5a0387c6a0436f144b7fc9b7c9f189a155b66c
RoKen1000 <rosskennedy993@outlook.com>
2023-10-04 13:47:22 +0100

    Create separate form-validation and refresh-buttons javascript files to make scripts reusable. Removed script where possible from the PresentIndicative ItalianController view. Functionality successfully still working with scripts separated into separate files.

---
+++ LanguagePractice/wwwroot/js/form-validation.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    //const conjugations = @Html.Raw(Json.Serialize(Model));
+
+    $("#conj-form").on("submit", function (event) {
+        event.preventDefault();
+
+        $("#conj-form :input:not(:submit)").each(function () {
+            if (this.value.toLowerCase() === conjugations[this.id]) {
+                $("small#" + this.id).html("Correct!").css("color", "green")
+            } else {
+                $("small#" + this.id).html("Incorrect. Try again.").css("color", "red");
+            }
+        });
+
+    })
+})
