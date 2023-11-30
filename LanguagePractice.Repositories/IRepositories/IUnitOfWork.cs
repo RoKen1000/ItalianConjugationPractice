a955e3c70bd189cc51649315c80070128d7b01e8
RoKen1000 <rosskennedy993@outlook.com>
2023-11-30 16:07:34 +0000

    Create IUnitOfWork and UnitOfWork implementation class. Hopefully this will avoid the need to have three separate instances of WordManagementRepository and only one UnitOfWork service will need to be registered and injected.

---
+++ LanguagePractice.Repositories/IRepositories/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using LanguagePracticeSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguagePractice.Repositories.IRepositories
+{
+    public interface IUnitOfWork
+    {
+        IWordManagementRepository<PresentIndicative> PresentIndicative { get; }
+        IWordManagementRepository<PresentPerfect> PresentPerfect { get; }
+        IWordManagementRepository<Imperfect> Imperfect { get; }
+    }
+}
---
+++ LanguagePractice.Repositories/UnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using LanguagePractice.DataAccess.DataContext;
+using LanguagePractice.Repositories.IRepositories;
+using LanguagePracticeSite.Models;
+
+namespace LanguagePractice.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public IWordManagementRepository<PresentIndicative> PresentIndicative { get; private set; }
+        public IWordManagementRepository<PresentPerfect> PresentPerfect { get; private set; }
+        public IWordManagementRepository<Imperfect> Imperfect { get; private set; }
+        private readonly WordDatabaseContext _db;
+
+        public UnitOfWork(WordDatabaseContext db)
+        {
+            _db = db;
+            PresentIndicative = new WordManagementRepository<PresentIndicative>(_db);
+            PresentPerfect = new WordManagementRepository<PresentPerfect>(_db);
+            Imperfect = new WordManagementRepository<Imperfect>(_db);
+        }
+    }
+}
