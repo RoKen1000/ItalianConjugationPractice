6d0896f5f4cf092e77686243340442ef445260bd
RoKen1000 <rosskennedy993@outlook.com>
2023-11-30 12:03:31 +0000

    Create IRepositories folder. Create IItalianRepository.

---
+++ LanguagePractice.Repositories/IRepositories/IItalianRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguagePractice.Repositories.IRepositories
+{
+    internal interface IItalianRepository<T> where T : class
+    {
+        int ProduceRandomOffset(string verbTense);
+        void PresentIndicative();
+        void PresentPerfect();
+        void Imperfect();
+    }
+}
