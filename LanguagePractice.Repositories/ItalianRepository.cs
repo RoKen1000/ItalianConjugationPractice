e8509b8c2b6349bb2889de583931fb4cc33339b7
RoKen1000 <rosskennedy993@outlook.com>
2023-11-30 12:06:02 +0000

    Create ItalianRepository implementation class. Remove uneeded using statement at top of ItlaianRepository and IItalianRepository.

---
+++ LanguagePractice.Repositories/ItalianRepository.cs
@@ -0,0 +1,27 @@
+﻿using LanguagePractice.Repositories.IRepositories;
+
+namespace LanguagePractice.Repositories
+{
+    public class ItalianRepository<T> : IItalianRepository<T> where T : class
+    {
+        public void Imperfect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PresentIndicative()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PresentPerfect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ProduceRandomOffset(string verbTense)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
