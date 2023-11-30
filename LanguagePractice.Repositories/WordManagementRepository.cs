6884fbed1de675ab6139252a41b1e0c2f85ba218
RoKen1000 <rosskennedy993@outlook.com>
2023-11-30 13:31:26 +0000

    Injext WordDatabaseContext into WordMangementRepository. Create dbSet to allow for generic types to be used to query different tables in database.

--- LanguagePractice.Repositories/WordManagementRepository.cs
+++ LanguagePractice.Repositories/WordManagementRepository.cs
@@ -1,10 +1,21 @@
-﻿using LanguagePractice.Repositories.IRepositories;
+﻿using LanguagePractice.DataAccess.DataContext;
+using LanguagePractice.Repositories.IRepositories;
+using Microsoft.EntityFrameworkCore;
 using System.Linq.Expressions;
 
 namespace LanguagePractice.Repositories
 {
     public class WordManagementRepository<T> : IWordManagementRepository<T> where T : class
     {
+        private readonly WordDatabaseContext _db;
+        internal DbSet<T> dbSet;
+
+        public WordManagementRepository(WordDatabaseContext db)
+        {
+            _db = db;
+            this.dbSet = _db.Set<T>();
+        }
+
         public void Create(T word)
         {
             throw new NotImplementedException();
