5a14ed2c45c7ef81127e5aa7d1f9031dc6b054da
RoKen1000 <rosskennedy993@outlook.com>
2023-12-05 11:23:12 +0000

    Add code into script to remove the disabled attribute to the next word button only when all six correct answers are given. Tested working on PresentIndicative view.

--- LanguagePractice/wwwroot/js/next-word-button.js
+++ LanguagePractice/wwwroot/js/next-word-button.js
@@ -1,1 +1,17 @@
 ﻿
+$(".submit-buttons").on("click", function () {
+    setTimeout(() => {
+
+        let correctAnswers = 0;
+
+        const smallClasses = $("#conj-form").find("small").each(function () {
+            if ($(this).attr("class") === "correct-animation") {
+                correctAnswers++;
+            }
+        });
+
+        if (correctAnswers === 6) {
+            $("#next-word").removeAttr("disabled");
+        }
+    }, 50)
+})
