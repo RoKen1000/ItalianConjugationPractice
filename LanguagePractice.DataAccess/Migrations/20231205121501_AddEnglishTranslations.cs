120110a1e685a4c7674dc33e8e29f43779fb6ad4
RoKen1000 <rosskennedy993@outlook.com>
2023-12-05 12:19:03 +0000

    Create data migrations to add EnglishTranslation property to entity classes and tables. Success. Install required NuGet package in LanguagePracticeSite to make EF Core create the migrations.

---
+++ LanguagePractice.DataAccess/Migrations/20231205121501_AddEnglishTranslations.cs
@@ -0,0 +1,261 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LanguagePracticeSite.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEnglishTranslations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EnglishTranslation",
+                table: "PresentPerfectPhrases",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "EnglishTranslation",
+                table: "PresentIndicativeWords",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "EnglishTranslation",
+                table: "ImperfectWords",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "ImperfectWords",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "EnglishTranslation",
+                value: "to see/look at");
+
+            migrationBuilder.UpdateData(
+                table: "ImperfectWords",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "EnglishTranslation",
+                value: "to talk");
+
+            migrationBuilder.UpdateData(
+                table: "ImperfectWords",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "EnglishTranslation",
+                value: "to come");
+
+            migrationBuilder.UpdateData(
+                table: "ImperfectWords",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "EnglishTranslation",
+                value: "to do/make");
+
+            migrationBuilder.UpdateData(
+                table: "ImperfectWords",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "EnglishTranslation",
+                value: "to go");
+
+            migrationBuilder.UpdateData(
+                table: "ImperfectWords",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "EnglishTranslation",
+                value: "to live");
+
+            migrationBuilder.UpdateData(
+                table: "ImperfectWords",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "EnglishTranslation",
+                value: "to sleep");
+
+            migrationBuilder.UpdateData(
+                table: "ImperfectWords",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "EnglishTranslation",
+                value: "to ask");
+
+            migrationBuilder.UpdateData(
+                table: "ImperfectWords",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "EnglishTranslation",
+                value: "to put");
+
+            migrationBuilder.UpdateData(
+                table: "ImperfectWords",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "EnglishTranslation",
+                value: "to dress");
+
+            migrationBuilder.UpdateData(
+                table: "PresentIndicativeWords",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "EnglishTranslation",
+                value: "to do/make");
+
+            migrationBuilder.UpdateData(
+                table: "PresentIndicativeWords",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "EnglishTranslation",
+                value: "to see/look at");
+
+            migrationBuilder.UpdateData(
+                table: "PresentIndicativeWords",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "EnglishTranslation",
+                value: "to talk");
+
+            migrationBuilder.UpdateData(
+                table: "PresentIndicativeWords",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "EnglishTranslation",
+                value: "to come");
+
+            migrationBuilder.UpdateData(
+                table: "PresentIndicativeWords",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "EnglishTranslation",
+                value: "to go");
+
+            migrationBuilder.UpdateData(
+                table: "PresentIndicativeWords",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "EnglishTranslation",
+                value: "to live");
+
+            migrationBuilder.UpdateData(
+                table: "PresentIndicativeWords",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "EnglishTranslation",
+                value: "to sleep");
+
+            migrationBuilder.UpdateData(
+                table: "PresentIndicativeWords",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "EnglishTranslation",
+                value: "to ask");
+
+            migrationBuilder.UpdateData(
+                table: "PresentIndicativeWords",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "EnglishTranslation",
+                value: "to put");
+
+            migrationBuilder.UpdateData(
+                table: "PresentIndicativeWords",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "EnglishTranslation",
+                value: "to dress");
+
+            migrationBuilder.UpdateData(
+                table: "PresentPerfectPhrases",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "EnglishTranslation",
+                value: "to prepare");
+
+            migrationBuilder.UpdateData(
+                table: "PresentPerfectPhrases",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "EnglishTranslation",
+                value: "to sell");
+
+            migrationBuilder.UpdateData(
+                table: "PresentPerfectPhrases",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "EnglishTranslation",
+                value: "to go");
+
+            migrationBuilder.UpdateData(
+                table: "PresentPerfectPhrases",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "EnglishTranslation",
+                value: "to come");
+
+            migrationBuilder.UpdateData(
+                table: "PresentPerfectPhrases",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "EnglishTranslation",
+                value: "to do/make");
+
+            migrationBuilder.UpdateData(
+                table: "PresentPerfectPhrases",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "EnglishTranslation",
+                value: "to live");
+
+            migrationBuilder.UpdateData(
+                table: "PresentPerfectPhrases",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "EnglishTranslation",
+                value: "to sleep");
+
+            migrationBuilder.UpdateData(
+                table: "PresentPerfectPhrases",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "EnglishTranslation",
+                value: "to ask");
+
+            migrationBuilder.UpdateData(
+                table: "PresentPerfectPhrases",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "EnglishTranslation",
+                value: "to put");
+
+            migrationBuilder.UpdateData(
+                table: "PresentPerfectPhrases",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "EnglishTranslation",
+                value: "to dress");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EnglishTranslation",
+                table: "PresentPerfectPhrases");
+
+            migrationBuilder.DropColumn(
+                name: "EnglishTranslation",
+                table: "PresentIndicativeWords");
+
+            migrationBuilder.DropColumn(
+                name: "EnglishTranslation",
+                table: "ImperfectWords");
+        }
+    }
+}
