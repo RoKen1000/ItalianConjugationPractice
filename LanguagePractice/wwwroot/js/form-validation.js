da54d7e2981fff247e95f2fa5fd9fb8c77cffbb8
RoKen1000 <rosskennedy993@outlook.com>
2023-12-07 11:33:15 +0000

    Optimise form-validation script to adhere to DRY. Test and conjugation form validation still works as intended.

--- LanguagePractice/wwwroot/js/form-validation.js
+++ LanguagePractice/wwwroot/js/form-validation.js
@@ -4,38 +4,24 @@
         event.preventDefault();
 
         $("#conj-form :input:not(:submit)").each(function () {
+            let feminineForm = "";
+
             if (conjugations.usesEssere === true) {
 
                 let singularConjugations = ["firstPersonSingular", "secondPersonSingular", "thirdPersonSingular"];
                 let pluralConjugations = ["firstPersonPlural", "secondPersonPlural", "thirdPersonPlural"];
 
                 if (singularConjugations.includes(this.id)) {
-
-                    let feminineFormSingular = conjugations[this.id].slice(0, -1) + "a";
-
-                    if (this.value.toLowerCase() === conjugations[this.id] || this.value.toLowerCase() === feminineFormSingular) {
-                        $("small#" + this.id).html("Correct!").css("color", "green").addClass("correct-animation");
-                    } else {
-                        $("small#" + this.id).html("Incorrect. Try again.").css("color", "red").removeClass("correct-animation");
-                    }
+                    feminineForm = conjugations[this.id].slice(0, -1) + "a";
                 } else if (pluralConjugations.includes(this.id)) {
-
-                    let feminineFormPlural = conjugations[this.id].slice(0, -1) + "e";
-
-                    if (this.value.toLowerCase() === conjugations[this.id] || this.value.toLowerCase() === feminineFormPlural) {
-                        $("small#" + this.id).html("Correct!").css("color", "green").addClass("correct-animation");
-                    } else {
-                        $("small#" + this.id).html("Incorrect. Try again.").css("color", "red").removeClass("correct-animation");
-                    }
+                    feminineForm = conjugations[this.id].slice(0, -1) + "e";
                 }
-
             }
-            else {
-                if (this.value.toLowerCase() === conjugations[this.id]) {
-                    $("small#" + this.id).html("Correct!").css("color", "green").addClass("correct-animation");
-                } else {
-                    $("small#" + this.id).html("Incorrect. Try again.").css("color", "red").removeClass("correct-animation");
-                }
+
+            if (this.value.toLowerCase() === conjugations[this.id] || this.value.toLowerCase() === feminineForm) {
+                $("small#" + this.id).html("Correct!").css("color", "green").addClass("correct-animation");
+            } else {
+                $("small#" + this.id).html("Incorrect. Try again.").css("color", "red").removeClass("correct-animation");
             }
         });
 
