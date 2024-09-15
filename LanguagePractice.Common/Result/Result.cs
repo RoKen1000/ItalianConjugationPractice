8da697f47a96725c00377cc385af8dcee70d2ba7
RoKen1000 <rosskennedy993@outlook.com>
2024-09-15 10:16:30 +0100

    Create common project and Result class.

---
+++ LanguagePractice.Common/Result/Result.cs
@@ -0,0 +1,29 @@
+﻿namespace LanguagePractice.Common.Result
+{
+    public class Result
+    {
+        public Result()
+        {
+            IsSuccess = true;
+        }
+
+        public Result(string error)
+        {
+            IsSuccess = false;
+            ErrorMessage = error;
+        }
+
+        private bool IsSuccess { get; set; }
+        private string? ErrorMessage { get; set; }
+
+        public static Result Success()
+        {
+            return new Result();
+        }
+
+        public static Result Failure(string errorMessage)
+        {
+            return new Result(errorMessage);
+        }
+    }
+}
